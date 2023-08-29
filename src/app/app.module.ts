3b16cfce1327092dfa6b410b3c4d4a9c81e52167
LucasZDO <erickrucke1@gmail.com>
2023-08-29 11:21:05 -0300

    primeiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,23 @@
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(
+    {
+      next: products => this.products = products
+    }
+    )
+}
+}
